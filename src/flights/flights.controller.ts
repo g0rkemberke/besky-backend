e487791310ee9e914d4592475d8e1e972eba063e
Görkem Tutkun <gorkemtutkun@Mac.home>
2026-03-11 14:06:29 +0300

    Admin guvenlik ve token algoritmasi guncellendi

--- src/flights/flights.controller.ts
+++ src/flights/flights.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Headers, UnauthorizedException, UseGuards, Request } from '@nestjs/common';
 import { FlightsService } from './flights.service';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard'; // Güvenlik 
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('flights')
 export class FlightsController {
@@ -12,28 +12,34 @@ export class FlightsController {
   }
 
   @Get('booked')
-  findBooked(@Headers('x-admin-token') token: string) {
+  findBooked(@Headers() headers: Record<string, string>) {
+    // Güvenli Okuma: Tüm harf formatlarını kapsar
+    const token = headers['x-admin-token'] || headers['X-Admin-Token'];
     const SECRET_TOKEN = process.env.ADMIN_TOKEN || "besky_london_2026_secure";
+    
     if (token !== SECRET_TOKEN) {
       throw new UnauthorizedException('Geçersiz erişim anahtarı!');
     }
     return this.flightsService.findBookedFlights(); 
   }
 
-  //  userEmail artık dışarıdan girilemez, sadece güvenli JWT Token'dan okunur!
+  // userEmail artık dışarıdan girilemez, sadece güvenli JWT Token'dan okunur!
   @UseGuards(JwtAuthGuard)
   @Post('book')
   async book(@Request() req: any, @Body() body: { flightId: string }) {
-    // req.user, login olduğunda token'a koyduğumuz doğrulanmış veridir. Başkası adına işlem yapılamaz.
     const userEmail = req.user.email; 
     return this.flightsService.bookFlight(body.flightId, userEmail);
   }
 
   @Post('add-flight')
-  create(@Body() flightData: any, @Headers('x-admin-token') token: string) {
+  create(@Body() flightData: any, @Headers() headers: Record<string, string>) {
+    // Güvenli Okuma
+    const token = headers['x-admin-token'] || headers['X-Admin-Token'];
     const SECRET_TOKEN = process.env.ADMIN_TOKEN || "besky_london_2026_secure";
     
     if (token !== SECRET_TOKEN) {
+      // Eğer hala hata olursa, backend terminaline sebebini yazdıracak dedektif logumuz:
+      console.error(`[GÜVENLİK] Eşleşmeme Hatası -> Gelen Token: ${token} | Beklenen: ${SECRET_TOKEN}`);
       throw new UnauthorizedException('Geçersiz erişim anahtarı!');
     }
     
